8a29af2da51d5b7da1973d43b5779704c01ecdf5
KenilAvaiyaa <avaiyakekenil@gmail.com>
2024-05-02 16:11:36 +0530

    done

--- pointer animation/pointer.js
+++ pointer animation/pointer.js
@@ -1,2 +1,25 @@
 var main = document.querySelector(".main")
 var mouse = document.querySelector(".mouse")
+var image = document.querySelector(".img")
+
+main.addEventListener("mousemove",(dets)=>{
+    gsap.to(mouse,{
+        x:dets.x,
+        y:dets.y,
+        ease: "power4.out",
+        dration:0.5
+    })
+})
+
+image.addEventListener("mouseenter",()=>{
+    mouse.innerHTML="View More"
+    gsap.to(mouse,{
+        scale:2.5
+    })
+})
+image.addEventListener("mouseleave",()=>{
+    mouse.innerHTML=""
+    gsap.to(mouse,{
+        scale:1,
+    })
+})
